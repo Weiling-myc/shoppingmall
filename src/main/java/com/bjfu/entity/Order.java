968e7bcf08fbc682e7981c95638bfaf0f42c1eef
mayuchen <2660270038@qq.com>
2021-01-16 22:34:20 +0800

    myc11/16

--- src/main/java/com/bjfu/entity/Order.java
+++ src/main/java/com/bjfu/entity/Order.java
@@ -12,7 +12,7 @@ import java.util.Date;
 public class Order implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private Integer id;
     /**
